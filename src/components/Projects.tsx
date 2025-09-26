33df27b0b30e8435e16704ea44b7924ed787d788
jayaviswanathreddy <tjvreddy07@gmail.com>
2025-09-26 12:21:06 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -90,7 +90,7 @@ const Projects = () => {
                     className="border-primary text-primary hover:bg-primary hover:text-primary-foreground"
                     asChild
                   >
-                    <a href={project.githubUrl} target="https://github.com/jayaviswanathreddy/007.git" rel="https://github.com/jayaviswanathreddy/007.git">
+                    <a href={"https://github.com/jayaviswanathreddy/007.git"} target="https://github.com/jayaviswanathreddy/007.git" rel="https://github.com/jayaviswanathreddy/007.git">
                       <Github size={16} className="mr-2" />
                       Code
                     </a>
